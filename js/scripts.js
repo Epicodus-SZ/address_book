b188ce4d5d34c5bc28b3d07b75fc6a74e061f72e
Emilie Thoreson and Steve Zaske <emilie@>
2017-06-01 10:47:21 -0700

    improve design of contact form

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
     $("div#contactList").append("<button id='"+formName+"' class='btn btn-primary functionClick'>" + formName + "</button>");
 
-    $("div#contactCardSpace").append("<div class='contact' id='"+formName+"'><p>Name:" + formName + "</p><p>Address:" + formAddress + "</p></div>");
+    $("div#contactCardSpace").append("<div class='contact' id='"+formName+"'><h2>Contact:</h2><p>Name: " + formName + "</p><p>Address: " + formAddress + "</p></div>");
 
     $("button#"+formName).click(function(event) {
       $("div#"+formName).toggle();
